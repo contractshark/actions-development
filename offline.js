fe12eb151956b7acf2b53ee8f5fb54ead20853fd
sam bacha <sam@freighttrust.com>
2021-02-06 01:06:00 -0800

    feat(syscall): error reporting

---
+++ offline.js
@@ -0,0 +1,19 @@
+/** 
+* Offline Error Codes
+* @summary POSIX Error Codes
+* @exports {offlineErrorCodes}
+*/
+
+var offlineErrorCodes = [
+  'EAI_AGAIN',
+  'ENETDOWN',
+  'EPROTO',
+  'ENOPROTOOPT',
+  'EHOSTDOWN',
+  'ENONET',
+  'EHOSTUNREACH',
+  'EOPNOTSUPP',
+  'ENETUNREACH',
+]
+
+module.exports = offlineErrorCodes
